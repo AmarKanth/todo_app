14caceb567041aa9d417757a9de25c4b5bd63ac8
AmarKanth <amarkanth.bollu@gmail.com>
2024-04-19 17:02:01 +0530

    added alembic

---
+++ README.md
@@ -0,0 +1,4 @@
+# fastapi
+insert into todos (title, description, priority, complete) values('Go to the store', 'Pick up eggs', 5, False);
+insert into todos (title, description, priority, complete) values('Cut the lawn', 'Grass is getting long', 3, False);
+insert into todos (title, description, priority, complete) values('Feed the dog', 'He is getting hungry', 5, False);
---
+++ api/database.py
@@ -0,0 +1,21 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
